3e2ff709462bd0728e4a216ea5055f65b26236a4
hatimiti <hatti33@gmail.com>
2015-08-09 00:43:08 +0900

    クラスパスリソースのロード処理修正

--- doxls/src/main/java/com/github/hatimiti/doxsl/util/ClassPathResourceLoader.java
+++ doxls/src/main/java/com/github/hatimiti/doxsl/util/ClassPathResourceLoader.java
@@ -1,12 +1,64 @@
 package com.github.hatimiti.doxsl.util;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 import java.util.Optional;
 
 public class ClassPathResourceLoader {
 
-	public static Optional<InputStream> load(String path) {
-		return Optional.ofNullable(ClassLoader.getSystemResourceAsStream(path));
+	public static Optional<InputStream> load(String path) throws IOException {
+
+		Objects.requireNonNull(path);
+
+		if (path.startsWith("/")) {
+			path = path.substring(1);
+		}
+
+		InputStream is = getDefaultClassLoader().getResourceAsStream(path);
+		if (is == null) {
+			is = ClassLoader.getSystemResourceAsStream(path);
+		}
+
+		return Optional.ofNullable(is);
+	}
+
+	/**
+	 * Return the default ClassLoader to use: typically the thread context
+	 * ClassLoader, if available; the ClassLoader that loaded the ClassUtils
+	 * class will be used as fallback.
+	 * <p>Call this method if you intend to use the thread context ClassLoader
+	 * in a scenario where you clearly prefer a non-null ClassLoader reference:
+	 * for example, for class path resource loading (but not necessarily for
+	 * {@code Class.forName}, which accepts a {@code null} ClassLoader
+	 * reference as well).
+	 * @return the default ClassLoader (only {@code null} if even the system
+	 * ClassLoader isn't accessible)
+	 * @see Thread#getContextClassLoader()
+	 * @see ClassLoader#getSystemClassLoader()
+	 */
+	protected static ClassLoader getDefaultClassLoader() {
+		ClassLoader cl = null;
+		try {
+			cl = Thread.currentThread().getContextClassLoader();
+		}
+		catch (Throwable ex) {
+			// Cannot access thread context ClassLoader - falling back...
+		}
+		if (cl == null) {
+			// No thread context class loader -> use class loader of this class.
+			cl = ClassPathResourceLoader.class.getClassLoader();
+			if (cl == null) {
+				// getClassLoader() returning null indicates the bootstrap ClassLoader
+				try {
+					cl = ClassLoader.getSystemClassLoader();
+				}
+				catch (Throwable ex) {
+					// Cannot access system ClassLoader - oh well, maybe the caller can live with null...
+				}
+			}
+		}
+		return cl;
 	}
 
 }
